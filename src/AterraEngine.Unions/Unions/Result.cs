7642d6297321470202789e00edfff0c810c217a0
Anna Sas <anna.sas.dev@gmail.com>
2025-03-24 13:46:05 +0100

    Feat: Result unions

---
+++ src/AterraEngine.Unions/Unions/Result.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+namespace AterraEngine.Unions;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[UnionAliases("State", "Error")]
+[UnionExtra(UnionExtra.GenerateFrom | UnionExtra.GenerateAsValue)]
+public readonly partial struct Result() : IUnion<bool, Error<string>> {
+    public bool State => AsState;
+    public bool TryGetState(out bool state) => TryGetAsState(out state);
+
+    public static implicit operator bool(Result value) => value.AsState;
+
+    public static Result FromError(string failure) => FromError(new Error<string>(failure));
+}
+
+[UnionAliases("Success", "Error")]
+[UnionExtra(UnionExtra.GenerateFrom | UnionExtra.GenerateAsValue)]
+public readonly partial struct Result<T>() : IUnion<T, Error<string>> {
+    public static implicit operator bool(Result<T> value) => value.IsSuccess;
+    public static Result<T> FromError(string failure) => FromError(new Error<string>(failure));
+}
