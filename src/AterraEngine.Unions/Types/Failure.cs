66bfb10378b0613b379730ee08cb2dfc2f6045ed
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-07-27 21:09:19 +0200

    Refactor: Removed unused generic type parameter `<T>` from `Failure` struct documentation

--- src/AterraEngine.Unions/Types/Failure.cs
+++ src/AterraEngine.Unions/Types/Failure.cs
@@ -8,9 +8,6 @@ namespace AterraEngine.Unions;
 /// <summary>
 ///     Represents a failure state in a computation or operation, encapsulating a value of a specific type.
 /// </summary>
-/// <typeparam name="T">
-///     The type of the value encapsulated in the failure state.
-/// </typeparam>
 public readonly struct Failure {
     public static readonly Failure Empty = new();
 }
