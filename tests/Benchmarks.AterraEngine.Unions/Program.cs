bd81100a80a9a89af560e31c1a7cf7f052d24359
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-07-27 21:51:24 +0200

    Refactor: Removed `DiscriminatedUnionsBenchmarkRecursiveReturns`, cleaned up `Program.cs`, and updated `README.md` benchmarks sections

--- tests/Benchmarks.AterraEngine.Unions/Program.cs
+++ tests/Benchmarks.AterraEngine.Unions/Program.cs
@@ -9,8 +9,7 @@ namespace Benchmarks.AterraEngine.Unions;
 // ---------------------------------------------------------------------------------------------------------------------
 public static class Program {
     public static void Main(string[] args) {
-        // BenchmarkRunner.Run<DiscriminatedUnionsBenchmark>();
+        BenchmarkRunner.Run<DiscriminatedUnionsBenchmark>();
         // BenchmarkRunner.Run<DiscriminatedUnionsBenchmarkEnhanced>();
-        BenchmarkRunner.Run<DiscriminatedUnionsBenchmarkRecursiveReturns>();
     }
 }
